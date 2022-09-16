0f24cbecab6b63ccf80b5e0c9967c28adafac14c
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-09-16 08:27:07 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+    static int factorial(int number)
+    {
+        int factorial = 1;
+        while(number > 1)
+        {
+            factorial = number*factorial;
+            number--;
+        }
+
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(factorial(a));
+
+    }
+}
