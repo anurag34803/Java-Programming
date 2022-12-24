193b6d96ad1dddefcc193f777e290529ff1ee618
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-12-24 15:28:26 +0530

    Update Sum_Of_N_Natural_Number.java

--- Recursion/Sum_Of_N_Natural_Number.java
+++ Recursion/Sum_Of_N_Natural_Number.java
@@ -4,12 +4,8 @@ public class Sum_Of_N_Natural_Number{
 
     static int sum(int n)
     {
-        if(n==1)
-            return 1;
-
-        else if (n==0) {
-            return 0;
-        }
+        if(n==1 && n==0)
+            return n;
 
         return n + sum(n-1);
     }
